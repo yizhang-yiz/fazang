4b0c1d5d1c0bc31d59752c8d74f9a500c29094f1
Yi Zhang <yi.zhang6@iconplc.com>
2026-07-27 21:55:48 -0700

    add missing access module

---
+++ src/fazang.f90
@@ -0,0 +1,15 @@
+module fazang
+  use fz_env
+  use fz_var
+  use fz_eval
+
+  ! cvodes solve from sundials
+#ifdef USE_SUNDIALS
+  use fazang_cvodes_mod
+  use fazang_cvodes_model_mod
+  use fazang_cvodes_options_mod
+#endif
+
+  implicit none
+
+end module fazang
