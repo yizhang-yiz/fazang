bbfbc6770cd1cab58469d29aa29fd1edcdce2a2f
Yi Zhang <yi.zhang6@iconplc.com>
2026-08-01 11:04:10 -0700

    use internal procedure for each op's chain

--- src/vari.f90
+++ src/vari.f90
@@ -3,80 +3,83 @@
 
 ! OP: use vi as vari arg
 #define DEF_OP1(NAME, OP, DYDX) \
-  function NAME/**/_vi (vi) result(v1); \
-    implicit none; \
-    procedure(chain_op) :: chain_op1; \
-    type(vari), pointer :: vi, v1; \
-    v1 = OP; \
-    v1%chain = c_funloc( chain_/**/NAME ); \
-    call core_adstack%push(vi%i); \
-  end function; \
-  subroutine chain_/**/NAME (this); \
+function NAME/**/_vi (vi) result(v1); \
+  implicit none; \
+  type(vari), pointer :: vi, v1; \
+  v1 = OP; \
+  v1%chain = c_funloc( mychain ); \
+  call core_adstack%push(vi%i); \
+  contains; \
+  subroutine mychain (this); \
     implicit none; \
     type(vari), pointer :: this, a; \
     call recover(a, core_adstack%pop_int(this%i+visize)); \
     a%adj_ = a%adj_ + this%adj_ * DYDX; \
-  end subroutine
+  end subroutine mychain; \
+end function
 
 ! OP = certain operation using (va%val_, vb%val_ , a, b ...)
 #define DEF_OP2_VD(NAME, OP, DYDX) \
-  function NAME/**/_vi_d(va, b) result(v1); \
-    implicit none; \
-    type(vari), pointer, intent(in) :: va; \
-    real(rk), intent(in) :: b; \
-    type(vari), pointer :: v1; \
-    v1 = OP; \
-    v1%chain = c_funloc( chain_vd_/**/NAME ); \
-    call core_adstack%push(va%i); \
-    call core_adstack%push(b); \
-  end function; \
-  subroutine chain_vd_/**/NAME (this); \
+function NAME/**/_vi_d(va, b) result(v1); \
+  implicit none; \
+  type(vari), pointer, intent(in) :: va; \
+  real(rk), intent(in) :: b; \
+  type(vari), pointer :: v1; \
+  v1 = OP; \
+  v1%chain = c_funloc( mychain ); \
+  call core_adstack%push(va%i); \
+  call core_adstack%push(b); \
+  contains; \
+  subroutine mychain (this); \
     implicit none; \
     type(vari), pointer :: this, va; \
     real(rk) :: b; \
     call recover(va, core_adstack%pop_int(this%i+visize)); \
     b = core_adstack%pop_real(this%i+visize+iksize); \
     va%adj_ = va%adj_ + this%adj_ * DYDX; \
-  end subroutine
+  end subroutine mychain; \
+end function
 
 #define DEF_OP2_DV(NAME, OP, DYDX) \
-  function NAME/**/_d_vi(a, vb) result(v1); \
-    implicit none; \
-    type(vari), pointer, intent(in) :: vb; \
-    real(rk), intent(in) :: a; \
-    type(vari), pointer :: v1; \
-    v1 = OP; \
-    v1%chain = c_funloc( chain_dv_/**/NAME ); \
-    call core_adstack%push(vb%i); \
-    call core_adstack%push(a); \
-  end function; \
-  subroutine chain_dv_/**/NAME (this); \
+function NAME/**/_d_vi(a, vb) result(v1); \
+  implicit none; \
+  type(vari), pointer, intent(in) :: vb; \
+  real(rk), intent(in) :: a; \
+  type(vari), pointer :: v1; \
+  v1 = OP; \
+  v1%chain = c_funloc( mychain ); \
+  call core_adstack%push(vb%i); \
+  call core_adstack%push(a); \
+  contains; \
+  subroutine mychain (this); \
     implicit none; \
     type(vari), pointer :: this, vb; \
     real(rk) :: a; \
     call recover(vb, core_adstack%pop_int(this%i+visize)); \
     a = core_adstack%pop_real(this%i+visize+iksize); \
     vb%adj_ = vb%adj_ + this%adj_ * DYDX; \
-  end subroutine
+  end subroutine mychain; \
+end function
 
 #define DEF_OP2_VV(NAME, OP, DYDA, DYDB) \
-  function NAME/**/_vi_vi(va, vb) result(v1); \
-    implicit none; \
-    type(vari), pointer, intent(in) :: va, vb; \
-    type(vari), pointer :: v1; \
-    v1 = OP; \
-    v1%chain = c_funloc( chain_vv_/**/NAME ); \
-    call core_adstack%push(va%i); \
-    call core_adstack%push(vb%i); \
-  end function; \
-  subroutine chain_vv_/**/NAME (this); \
+function NAME/**/_vi_vi(va, vb) result(v1); \
+  implicit none; \
+  type(vari), pointer, intent(in) :: va, vb; \
+  type(vari), pointer :: v1; \
+  v1 = OP; \
+  v1%chain = c_funloc( mychain ); \
+  call core_adstack%push(va%i); \
+  call core_adstack%push(vb%i); \
+  contains; \
+  subroutine mychain (this); \
     implicit none; \
     type(vari), pointer :: this, va, vb; \
     call recover(va, core_adstack%pop_int(this%i+visize)); \
     call recover(vb, core_adstack%pop_int(this%i+visize+iksize)); \
     va%adj_ = va%adj_ + this%adj_ * DYDA; \
     vb%adj_ = vb%adj_ + this%adj_ * DYDB; \
-  end subroutine
+  end subroutine mychain; \
+end function
 
 #endif
 
